071ba1fcfd196f4b8c2b8417568c1e419439a452
Per Møldrup-Dalum <dalum@mac.com>
2012-09-14 22:36:03 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject my-counter "0.1.0-SNAPSHOT"
+  :description "First standalone test of a Clojure Hadoop job."
+  :url "http://theseus.dk/per"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [clojure-hadoop "1.4.1"]
+                 [org.apache.hadoop/hadoop-core "1.0.3"]])
